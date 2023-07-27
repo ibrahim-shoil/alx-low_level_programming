527971ee329c1885f5de15b0d5ccb28f21ecd8c7
ibrahim-shoil <ibrahimshoil50@gmail.com>
2023-07-28 02:29:31 +0300

    second

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,44 +1,28 @@
 #include "main.h"
-
 /**
-* rot13 -  encodes a string using rot13
-* @str:the string targeted
-*Return: returns the encoded string
-*/
-
-char *rot13(char *str)
+ * rot13 - change letters to ROT13.
+ * @s: analized string.
+ *
+ * Return: String with all letters in ROT13 base.
+ */
+char *rot13(char *s)
 {
-	int index1, index2;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int j;
 
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-											 'G', 'H', 'I', 'J', 'K', 'L',
-											 'M', 'N', 'O', 'P', 'Q', 'R',
-											 'S', 'T', 'U', 'V', 'W', 'X',
-											 'Y', 'Z', 'a', 'b', 'c', 'd',
-											 'e', 'f', 'g', 'h', 'i', 'j',
-											 'k', 'l', 'm', 'n', 'o', 'p',
-											 'q', 'r', 's', 't', 'u', 'v',
-											 'w', 'x', 'y', 'z'};
-
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-											 'T', 'U', 'V', 'W', 'X', 'Y',
-											 'Z', 'A', 'B', 'C', 'D', 'E',
-											 'F', 'G', 'H', 'I', 'J', 'K',
-											 'L', 'M', 'n', 'o', 'p', 'q',
-											 'r', 's', 't', 'u', 'v', 'w',
-											 'x', 'y', 'z', 'a', 'b', 'c',
-											 'd', 'e', 'f', 'g', 'h', 'i',
-											 'j', 'k', 'l', 'm'};
-	while (str[++index1])
+	while (*(s + i) != '\0')
 	{
-		for (index2 = 0; index2 < 52; index2++)
+		for (j = 0; j <= 51; j++)
 		{
-			if (str[index1] == alphabet[index2])
+			if (*(s + i) == a[j])
 			{
-				str[index1] = rot13key[index2];
+				*(s + i) = rot[j];
 				break;
 			}
 		}
+		i++;
 	}
-	return (str);
+	return (s);
 }
